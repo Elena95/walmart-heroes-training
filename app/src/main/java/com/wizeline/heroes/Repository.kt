49ff61c157e8c349144cbb917ab316208c204589
Maria Vazquez Flores <maria.vazquez@Marias-MacBook-Pro.local>
2022-01-13 16:20:44 -0600

    Change return type getCharacteres

--- app/src/main/java/com/wizeline/heroes/Repository.kt
+++ app/src/main/java/com/wizeline/heroes/Repository.kt
@@ -1,7 +1,10 @@
 package com.wizeline.heroes
 
+import retrofit2.Call
+
+
 class Repository {
-    fun getCharacters(ts:String,apikey:String, hash:String){
-        NetworkClient.api.characters(ts, apikey, hash)
+    fun getCharacters(ts:String,apikey:String, hash:String):Call<Characters>{
+         return NetworkClient.api.characters(ts, apikey, hash)
     }
 }
